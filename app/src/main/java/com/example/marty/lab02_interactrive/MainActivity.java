116c3df97ebf1f6badef38d03e127660a6e59c08
linmarty2016 <marty@assota.com.tw>
2016-03-15 10:05:43 +0800

    +-

--- app/src/main/java/com/example/marty/lab02_interactrive/MainActivity.java
+++ app/src/main/java/com/example/marty/lab02_interactrive/MainActivity.java
@@ -6,9 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import java.text.NumberFormat;
-
 public class MainActivity extends AppCompatActivity {
+    private int mQuantity = 0;
+    private int mPrice = 5;
+    private final String mNT$ = "NT$";
+    private StringBuilder mTotalPriceMessage = new StringBuilder(mNT$);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,25 +19,50 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        display(2);
+
+        displayTotalPrice();
+
     }
 
-    private void display(int number) {
+    private void displayTotalPrice() {
 
-       TextView quantityTextView =  (TextView)findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(String.valueOf(number));
-       TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
-        int price = 5;
-        int total = price *number;
-        String myString = NumberFormat.getCurrencyInstance().format(total);
-        priceTextView.setText(myString);
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
+        int total = mPrice * mQuantity;
+        int startIndex = mNT$.length();
+        int endIndex =  mTotalPriceMessage.length();
+        mTotalPriceMessage.delete(startIndex, endIndex).append(total);
+       // String myString = NumberFormat.getCurrencyInstance().format(total);
+        priceTextView.setText(mTotalPriceMessage);
+        String myString2 = "Thank You!";
+        if(mQuantity == 0) {
+            myString2 = "Free";
+        }
+        TextView okTextView = (TextView)findViewById(R.id.ok_text_view);
+        okTextView.setText(myString2);
+
+
+
+    }
+
+    private void displayMsg() {
+
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
+        priceTextView.setText("");
+        TextView okTextView = (TextView)findViewById(R.id.ok_text_view);
+        okTextView.setText("");
+    }
+
+    private void displayQuantity() {
+
+        TextView quantityTextView =  (TextView)findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(String.valueOf(mQuantity));
     }
 
     public void submitAdd(View view) {
 
-        int quanitty = getQuanitty();
-        display(++quanitty);
-
+       ++mQuantity;
+       displayQuantity();
+        displayMsg();
 
     }
 
@@ -44,13 +71,15 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
         String quantityString = quantityTextView.getText().toString();
         return Integer.parseInt(quantityString);
+
     }
 
     public void submitSub(View view) {
-        int quanitty = getQuanitty();
-        if(quanitty > 0) {
-            display(--quanitty);
 
+        if(mQuantity > 0) {
+            --mQuantity;
+            displayQuantity();
+            displayMsg();
         }
 
     }
