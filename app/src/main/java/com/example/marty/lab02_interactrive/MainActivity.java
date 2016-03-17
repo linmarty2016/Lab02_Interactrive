05d972722979fc65c033bc8344a36682250f8c22
linmarty2016 <marty@assota.com.tw>
2016-03-17 10:08:48 +0800

    加泡菜

--- app/src/main/java/com/example/marty/lab02_interactrive/MainActivity.java
+++ app/src/main/java/com/example/marty/lab02_interactrive/MainActivity.java
@@ -4,18 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int mQuantity = 0;
     private int mPrice = 5;
+    private int mToppingPrice = 50;
     private final String mNT$ = "NT$";
-    private StringBuilder mTotalPriceMessage = new StringBuilder(mNT$);
+    private StringBuilder mTotalPriceMessage = new StringBuilder("NT$" + mToppingPrice);
+    private StringBuilder mTotalPriceMessage2 = new StringBuilder("");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayMsg();
     }
 
     public void submitOrder(View view) {
@@ -25,16 +29,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayTotalPrice() {
-
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.Toppings_CheckBox_view);
         TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
-        int total = mPrice * mQuantity;
+        int total = mToppingPrice * mQuantity ;
         int startIndex = mNT$.length();
         int endIndex =  mTotalPriceMessage.length();
         mTotalPriceMessage.delete(startIndex, endIndex).append(total);
+
+        int start = 0;
+        int end = mTotalPriceMessage2.length();
        // String myString = NumberFormat.getCurrencyInstance().format(total);
-        priceTextView.setText(mTotalPriceMessage);
+        mTotalPriceMessage2 = mTotalPriceMessage2.delete(start,end).append("Name:鳴人\n臭豆腐\n加泡菜?"+ (checkBox.isChecked()  ? "true" : "false")
+                + (total > 0 ? "\nQuanity:"+ String.valueOf(mQuantity) + "\n"+mTotalPriceMessage : "" ));
+        priceTextView.setText(mTotalPriceMessage2);
         String myString2 = "Thank You!";
-        if(mQuantity == 0) {
+        if(total == 0) {
             myString2 = "Free";
         }
         TextView okTextView = (TextView)findViewById(R.id.ok_text_view);
@@ -45,9 +54,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayMsg() {
-
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.Toppings_CheckBox_view);
+        if (checkBox.isChecked()) {
+            mToppingPrice = 50;
+        }
+        else
+            {
+                mToppingPrice = 0;
+            }
+        mTotalPriceMessage = mTotalPriceMessage.delete(0,mTotalPriceMessage.length()).append("NT$" + mToppingPrice);
         TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
-        priceTextView.setText("");
+        priceTextView.setText(mTotalPriceMessage);
         TextView okTextView = (TextView)findViewById(R.id.ok_text_view);
         okTextView.setText("");
     }
@@ -61,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
     public void submitAdd(View view) {
 
        ++mQuantity;
+
        displayQuantity();
         displayMsg();
 
@@ -82,5 +100,24 @@ public class MainActivity extends AppCompatActivity {
             displayMsg();
         }
 
+    }
+
+    public void clickToppings(View view) {
+
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.Toppings_CheckBox_view);
+        if (checkBox.isChecked()) {
+            mToppingPrice = 50;
+            TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
+            int startIndex = mNT$.length();
+            int endIndex =  mTotalPriceMessage.length();
+            mTotalPriceMessage.delete(startIndex, endIndex).append(mToppingPrice);
+            // String myString = NumberFormat.getCurrencyInstance().format(total);
+            priceTextView.setText(mTotalPriceMessage);
+        }
+        else
+        {
+            mToppingPrice = 0;
+            displayMsg();
+        }
     }
 }
